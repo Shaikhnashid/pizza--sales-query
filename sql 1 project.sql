1ee2dc3794a042341fe8eb284f1b54a3225bfd9a
Shaikhnashid <97298698+Shaikhnashid@users.noreply.github.com>
2024-07-01 23:26:18 +0530

    Delete sql 1 project.sql

--- sql 1 project.sql
+++
@@ -1,9 +0,0 @@
--- Calculate the total revenue generated from pizza sales.
-
-SELECT 
-    ROUND(SUM(orders_details.quantity * pizzas.price),
-            0) AS Total_sales
-FROM
-    pizzas
-        JOIN
-    orders_details ON pizzas.pizza_id = orders_details.pizza_id
